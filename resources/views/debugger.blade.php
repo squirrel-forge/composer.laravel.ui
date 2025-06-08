5cd6c23ea27f770679aea0d63b033cef625e3d75
dasiux <me@siux.info>
2025-06-08 17:04:09 +0200

    clean debugger

--- resources/views/debugger.blade.php
+++ resources/views/debugger.blade.php
@@ -1,12 +1,10 @@
 <dialog open>
-    <h4>squirrel-forge ui v{{ \SquirrelForge\Laravel\Ui\Service::VERSION }}</h4>
-    <p><strong>Locale:</strong> <em>{!! app()->currentLocale() !!}</em></p>
-    <p><strong>Params:</strong></p>
-    <pre><code>GET {!! var_export($_GET, true) !!}</code></pre>
-    <pre><code>POST {!! var_export($_POST, true) !!}</code></pre>
-    <p><strong>User:</strong></p>
-    <pre><code>{!! var_export(Auth::user()?->toArray(), true) !!}</code></pre>
-    <p><strong>Session:</strong></p>
-    <pre><code>{!! var_export(session()->all(), true) !!}</code></pre>
+    <h4>squirrel-forge ui debugger</h4>
+    <p>sqf-ui v{{ \SquirrelForge\Laravel\Ui\Service::VERSION }}</p>
+    <code>@dump([
+        'LOCALE' => app()->currentLocale(),
+        'USER' => Auth::user()?->toArray(),
+        'SESSION' => ['ID' => session()->getId(), 'DATA' => session()->all()],
+    ])</code>
     <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
 </dialog>
